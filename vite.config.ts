f230df58ba10aa994830fc3264ca97599d5f4374
fabo <fabo.dev.email@gmail.com>
2024-12-25 18:43:05 +0100

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), compression({
+    algorithm: 'gzip', // Può essere 'gzip' o 'brotliCompress'
+    ext: '.gz', // Estensione dei file compressi
+    deleteOriginFile: false, // Mantiene anche i file originali
+    threshold: 10240, // Comprimi solo file più grandi di 10KB
+  }),
+  compression({
+    algorithm: 'brotliCompress', // Abilita anche Brotli
+    ext: '.br',
+    deleteOriginFile: false,
+    threshold: 10240,
+  }),
+  ],
+  build: {
+    minify: 'esbuild', // Puoi usare 'terser' o 'esbuild' (opzione predefinita)
+    terserOptions: {
+      compress: {
+        drop_console: true, // Rimuove i console.log per una build più pulita
+        drop_debugger: true, // Rimuove i debugger
+      },
+    },
+    cssCodeSplit: true, // Ottimizza i CSS generando file separati
+    sourcemap: false, // Disabilita le sourcemap in produzione
+  },
+});
